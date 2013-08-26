cbc30b845770e7c5b4448507116d36be0ed113a8
Samuel Morello <samuel.morello@ouvanous.com>
2013-08-26 15:51:59 +0200

    2013-08-26 15:51

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj4store "0.1.51"
+(defproject clj4store "0.1.52"
   :description "Simple HTTP client for 4store"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http.async.client "0.5.2"]
-                 [org.clojure/data.json "0.2.1"]
+                 [org.clojure/data.json "0.2.2"]
                  [org.clojure/data.zip "0.1.1"]])
 
